87f449eab9f875ea7b7901abab41d423dfda1f5f
Tobi <tobiawolaju21@gmail.com>
2024-03-20 16:18:02 +0100

    bkj,l

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http')
+http
+    .get('http://api.open-notify.org/astros.json', resp => {
+        let data = ''
+        resp.on('data', chunk => {
+            data += chunk
+        })
+        resp.on('end', () => {
+            let peopleData = JSON.parse(data)
+            console.log(peopleData)
+        })
+    })
